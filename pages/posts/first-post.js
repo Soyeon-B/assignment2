fdfcd33f2f44c826fbde0ada929f62d62fdb2761
Soyeon-B <bsy427@naver.com>
2021-04-05 21:44:30 +0900

    upload files

---
+++ pages/posts/first-post.js
@@ -0,0 +1,14 @@
+import Link from 'next/Link'
+
+export default function FirstPost() {
+	return (
+	 <>
+	  <h1>Baek Soyeon</h1>
+	  <h2>
+	   <Link href="/">
+	    <a>Back to home</a>
+	   </Link>
+	  </h2>
+	 </>
+	)
+}
